302f7112b712e0c80f33775b529ec555a91ef8bc
Garrett Beard <beardgarrett077@gmail.com>
2021-02-03 04:08:13 -0600

    Added support for persistent cipher text

--- main.py
+++ main.py
@@ -3,20 +3,33 @@ import subCipher as subCipher
 import vigenereCipher as vigenereCipher
 import utils as utils
 
-shiftKey = None
-subKey = None
-vigenereKey = None
-vigenereItr = 0
+CIPHER_TEXT = None
+POSITIONS = None
+UNSUPPORTED_CHARS = None
+
+
+def updateCipherText():
+    global CIPHER_TEXT, POSITIONS, UNSUPPORTED_CHARS, CIPHER_TEXT
+    CIPHER_TEXT = utils.getCipherTextInput()
+    POSITIONS, UNSUPPORTED_CHARS, CIPHER_TEXT = utils.removeUnsupportedChars(CIPHER_TEXT)
+
 
 while True:
-    choice = input("What would you like to do?\n"
+    shiftKey = None
+    subKey = None
+    vigenereKey = None
+    vigenereItr = 0
+
+    choice = input("\nWhat would you like to do?\n"
                    "1: Cipher Text Analysis\n"
                    "2: Crack Cipher Text\n"
-                   "3: Exit\n"
+                   "3: Change Cipher Text\n"
+                   "4: Exit\n"
                    "Choice: ")
+
     if choice == '1':
         while True:
-            analysisType = input("What form of analysis would you like to perform?\n"
+            analysisType = input("\nWhat form of analysis would you like to perform?\n"
                                  "1: Frequency Analysis\n"
                                  "2: Index of Coincidence\n"
                                  "3: Kasiki Analysis\n"
@@ -26,49 +39,48 @@ while True:
             if analysisType == '4':
                 break
 
-            cipherText = utils.getCipherTextInput()
-            _, _, cipherText = utils.removeUnsupportedChars(cipherText)
+            if CIPHER_TEXT is None:
+                updateCipherText()
 
             if analysisType == '1':
-                freqMap = utils.freqAnalysis(cipherText)
+                freqMap = utils.freqAnalysis(CIPHER_TEXT)
                 freqMap = dict(sorted(freqMap.items(), key=lambda value: value[1], reverse=True))
                 print(freqMap)
             elif analysisType == '2':
-                IC = utils.ICAnalysis(cipherText)
+                IC = utils.ICAnalysis(CIPHER_TEXT)
                 print(IC)
             elif analysisType == '3':
-                _, _, GCDs = utils.kasiskiAnalysis(cipherText)
+                _, _, GCDs = utils.kasiskiAnalysis(CIPHER_TEXT)
                 GCDs = list(dict.fromkeys(GCDs.values()))
                 output = "Potential key lengths: "
                 for gcd in GCDs:
                     output += str(gcd) + ", "
-                print(output[:-2] + "\n")
+                print(output[:-2])
             else:
-                print("Invalid input!\n")
+                print("Invalid input!")
 
     elif choice == '2':
-        cipherText = utils.getCipherTextInput()
-        positions, unsupportedChars, cipherText = utils.removeUnsupportedChars(cipherText)
+        if CIPHER_TEXT is None:
+            updateCipherText()
 
-        freqList = sorted(utils.freqAnalysis(cipherText), key=utils.freqAnalysis(cipherText).get, reverse=True)
-        IC = utils.ICAnalysis(cipherText)
-        trigrams, indices, GCDs = utils.kasiskiAnalysis(cipherText)
+        freqList = sorted(utils.freqAnalysis(CIPHER_TEXT), key=utils.freqAnalysis(CIPHER_TEXT).get, reverse=True)
+        IC = utils.ICAnalysis(CIPHER_TEXT)
+        trigrams, indices, GCDs = utils.kasiskiAnalysis(CIPHER_TEXT)
         GCDs = list(dict.fromkeys(GCDs.values()))
 
-        print("IC: ", IC)
         if abs(IC - 0.0686) < 0.02:
-            print("Recommended cipher: Shift or Substitution\n")
+            print("Recommended cipher: Shift or Substitution")
         elif abs(IC - 0.038466) < 0.003:
-            print("Cipher text is likely random\n")
+            print("Cipher text is likely random")
         else:
-            print("Recommended cipher: Vigenere\n")
+            print("Recommended cipher: Vigenere")
 
         while True:
             cancel = False
             plainText = ''
 
             while True:
-                cipherType = input("What cipher would you like to try?\n"
+                cipherType = input("\nWhat cipher would you like to try?\n"
                                    "1: Shift\n"
                                    "2: Substitution\n"
                                    "3: Vigenere\n"
@@ -78,30 +90,30 @@ while True:
                 if cipherType == '1':
                     if shiftKey is None:
                         shiftKey = (utils.aToI[utils.naturalFreq[0]] - utils.aToI[freqList[0]]) % 26
-                    plainText = ''.join(utils.intToText(shiftCipher.decrypt(utils.textToInt(cipherText), shiftKey)))
+                    plainText = ''.join(utils.intToText(shiftCipher.decrypt(utils.textToInt(CIPHER_TEXT), shiftKey)))
                     break
                 elif cipherType == '2':
                     if subKey is None:
                         subKey = dict(zip(freqList, utils.naturalFreq))
-                    plainText = subCipher.decrypt(cipherText, subKey)
+                    plainText = subCipher.decrypt(CIPHER_TEXT, subKey)
                     break
                 elif cipherType == '3':
                     if vigenereKey is None:
-                        vigenereKey = vigenereCipher.genKey(cipherText, GCDs, vigenereItr)
+                        vigenereKey = vigenereCipher.genKey(CIPHER_TEXT, GCDs, vigenereItr)
                         vigenereItr += 1
-                    plainText = ''.join(utils.intToText(vigenereCipher.decrypt(utils.textToInt(cipherText), vigenereKey)))
+                    plainText = ''.join(utils.intToText(vigenereCipher.decrypt(utils.textToInt(CIPHER_TEXT), vigenereKey)))
                     break
                 elif cipherType == '4':
                     cancel = True
                     break
                 else:
-                    print("Invalid input!\n")
+                    print("Invalid input!")
 
             if cancel:
                 break
 
-            formattedText = utils.addUnsupportedChars(plainText, positions, unsupportedChars)
-            print("Cipher text: ", cipherText)
+            formattedText = utils.addUnsupportedChars(plainText, POSITIONS, UNSUPPORTED_CHARS)
+            print("Cipher text: ", CIPHER_TEXT)
             print(" Plain text: ", formattedText)
 
             correct = input("Does this look correct? (y/n): ")
@@ -119,11 +131,15 @@ while True:
                     shiftKey = (shiftKey + 1) % 26
                     if shiftKey == 0:
                         shiftKey = 1
-                    print('Key updated! Try shift cipher again.\n')
+                    print('Key updated! Try shift cipher again.')
                 elif cipherType == '2':
                     subKey = subCipher.genKey(subKey)
                 elif cipherType == '3':
-                    vigenereKey = vigenereCipher.genKey(cipherText, GCDs, vigenereItr)
+                    vigenereKey = vigenereCipher.genKey(CIPHER_TEXT, GCDs, vigenereItr)
                     vigenereItr += 1
     elif choice == '3':
+        updateCipherText()
+    elif choice == '4':
         break
+    else:
+        print("Invalid input!")
